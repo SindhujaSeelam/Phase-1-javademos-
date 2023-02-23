5c92e4c4a2d1f1d866f8a3ce1d42951b046682d1
Sindhuja <ssindhuja@teksystems.com>
2023-02-23 12:56:17 +0530

    programs

---
+++ javafsd/src/javafsd/staticex.java
@@ -0,0 +1,13 @@
+package javafsd;
+
+public class staticex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Main method");
+method();
+	}
+static  void method(){
+	System.out.println("Static Block");
+}
+}
