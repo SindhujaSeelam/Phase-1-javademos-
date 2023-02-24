b27868c9613e7f96d99b9f86ee61ce25048b306c
Sindhuja <ssindhuja@teksystems.com>
2023-02-24 09:40:30 +0530

    programs

---
+++ javafsd/src/javafsd/accesscar.java
@@ -0,0 +1,12 @@
+package javafsd;
+
+public class accesscar {
+
+	public static void main(String[] args) {
+		car c =new car();
+		System.out.println(c.nofwheels);
+		System.out.println(c.colour);
+
+	}
+
+}
---
+++ javafsd/src/javafsd/car.java
@@ -0,0 +1,14 @@
+package javafsd;
+
+public class car {
+	int nofwheels;
+	String colour;
+	car(){
+		nofwheels=4;
+		colour="Black";
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
