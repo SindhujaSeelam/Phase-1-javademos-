d56ec5aa929bc9dc5b41a20e7b6b64fe6884e3c1
Sindhuja <ssindhuja@teksystems.com>
2023-02-24 10:23:44 +0530

    programs

---
+++ javafsd/src/javafsd/datatypesdemo.java
@@ -0,0 +1,19 @@
+package javafsd;
+public class datatypesdemo {
+	static byte a=10;
+public static void main(String[] args) {
+	a=(byte)150;
+	System.out.println("a="+a);
+	short b=150;
+	int c=1000;
+	float d=5.6f;
+	double e=213214.23;
+	System.out.println(Byte.MIN_VALUE);
+	System.out.println(Byte.MAX_VALUE);
+	System.out.println(Short.MIN_VALUE);
+	System.out.println(Short.MAX_VALUE);
+	System.out.println(Float.MAX_VALUE);
+	System.out.println(Double.MAX_VALUE);
+	
+}
+}
